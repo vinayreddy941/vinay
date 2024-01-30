8c250001c1c5899ee4c08546e9b8c22cfbdb6309
vinayreddy941 <vinayeee2001@gmail.com>
2024-01-30 18:06:01 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
-function App() {
+import { useNavigate } from "react-router-dom";
+
+
+
+
+
+const SignIn = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main>
+      <div>
+       
+        <div className="image-container">
+          <img className="image" src="#" alt="Logo" />
+        </div>
+        <div className="container">
+          <div className="signin">
+            <h2>Sign In</h2>
+            <h4>Sign in to your Account</h4>
+          </div>
+          <div className="content">
+            <form>
+              <label htmlFor="username">Username or Email address</label>
+              <input type="text" id="username" name="username" required />
 
-export default App;
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" required />
+
+              <p className="forgot">
+                <a href="#">Forgot password?</a>
+              </p>
+              <button type="submit" className="signin-container green">
+                Sign In
+              </button>
+              <h4>
+                Don't have an account? <a href="#">Register here</a>
+              </h4>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+      
+      </div>
+    </main>
+  );
+};
+
+export default SignIn;
